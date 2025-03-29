d11cdcd4d2d8c09e78d723ec3c19c27adae0469f
Sora3347 <gabrielribcerqueira@gmail.com>
2025-03-29 15:56:44 -0400

    added .gitignore

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll("button");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
         let hello_div = document.getElementById("hello_div");
-        if (hello_div.style.visibility == "hidden") {
+        if (hello_div.style.visibility === "hidden") {
             hello_div.style.visibility = "visible";
         } else {
             hello_div.style.visibility = "hidden";
